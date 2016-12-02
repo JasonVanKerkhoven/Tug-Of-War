a5d97aaae877df52d4a4d1e34156c80974571001
Jason Van Kerkhoven <jason.vk@outlook.com>
2016-12-02 12:28:06 -0500

    cleaned up pbl.v

--- src/pbl.v
+++ src/pbl.v
@@ -15,40 +15,28 @@
 `timescale 1ns / 1ps
 
 module pbl (pbl, pbr, rst, clr, push, tie, right);
+
+	//module I/O
+	input pbr;
+	input pbl;
+	input clr;
+	input rst;
+	output push;
+	output tie;
+	output right;
 	
-	/**
-	//Declaring inputs/outputs
-	input pbl, pbr, rst, clr;
-	output push, tie, right;
+	//internal wires/regs
+	wire G,H,Gx,Hx;
 	
-	//delcaring wires
-	wire leftpushed, rightpushed, H, G;
-	
-	//declaring modules
-	latch latchL(.trigger(left_pushed), .clr(clr), .out(G));
-	latch latchR(.trigger(right_pushed), .clr(clr), .out(H));
-	
-	//gate logic for PBL
-	assign leftpushed = pbl & !H;
-	assign rightpushed = pbr & !G;
-	assign push = (pbl | pbr) & ~clr & ~rst;
-	assign tie = pbl & pbr & ~rst;
-	assign right = pbr & ~G & ~clr & ~rst;
-	*/
-	
-	input pbr,pbl,clr,rst; //inputs
-	output push,tie,right; // outputs
-	wire G,H,Gx,Hx; //wires used to make the logic
-	
-	//PBL logic  
+	//latch signals
 	assign Gx = (~H)&pbl; 
 	assign Hx = (~G)&pbr;
 	assign G = (G|Gx)&(~(rst|clr));	
 	assign H = (H|Hx)&(~(rst|clr));	
 	
-	//outputs we want in this module
-	assign push = G|H; //Either player pushed button
-	assign tie = G&H; // both player pushed simultaneously
-	assign right = H&(~G); //1 if right player pushed first
+	//gatewise logic for PBL output
+	assign push = G|H;
+	assign tie = G&H;
+	assign right = H&(~G);
 	
 endmodule
