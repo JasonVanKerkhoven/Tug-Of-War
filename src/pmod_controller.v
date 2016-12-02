818c70ff93a79e2740cd2f40c758c1a35b02b718
Jason Van Kerkhoven <jason.vk@outlook.com>
2016-12-02 12:32:32 -0500

    cleaned up pmod_controller.v

--- src/pmod_controller.v
+++ src/pmod_controller.v
@@ -26,6 +26,7 @@ module pmod_controller(rst, pbr, pbl, CLK_I, pmod_speaker, pmod_gain, pmod_enabl
 	output pmod_enable;
 	
 	//constants
+	//clock 100Mhz / 700Hz ---> get a 700Hz PWM using counter from 0-max
 	parameter max = 100000000/700;
 	
 	//internal regs/wires
@@ -44,7 +45,6 @@ module pmod_controller(rst, pbr, pbl, CLK_I, pmod_speaker, pmod_gain, pmod_enabl
 		//play 700Hz sound when user presses rst or whe  the user wins
 		if(rst == 1 || pbl == 1 || pbr == 1)
 		begin
-			//counter <= 0;
 			pmod_gain <= 0;
 			pmod_enable <= 1;
 			pmod_speaker <= 1;
@@ -65,75 +65,4 @@ module pmod_controller(rst, pbr, pbl, CLK_I, pmod_speaker, pmod_gain, pmod_enabl
 		end
 	end
 endmodule
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	/*
-	//define constants
-	parameter WL =7'b1110000;
-	parameter WR = 7'b0000111;
-	
-	//module inputs
-	input [6:0] score;	//  MSB 5 [WL L2 L1 0 R1 R2 WR] LSB 0
-	input CLK_I;
-	input rst;
-	
-	//module outputs
-	output reg pmod_speaker;
-	output reg pmod_gain = 1;
-	output reg pmod_enable;
-	
-	parameter max = 100000000/300;
-	
-	//module internal regs/wires
-	reg [4:0] counter;
 
-
-	always @ (posedge CLK_I)
-	begin
-		if (rst ==1)	pmod_enable = 1;
-		else 				pmod_enable = 0;
-	end
-	
-	always @ (posedge CLK_I)
-	begin
-		if (counter == max) counter <= 0;
-		else begin
-			counter <= counter+1;
-			pmod_speaker = ~pmod_speaker;
-		end
-	end
-
-	always @ (posedge rst or posedge CLK_I) begin
-		if (rst) pmod_enable = 0;
-		
-		else if (CLK_I) begin
-			if (counter == max) begin 
-				counter <= 0;
-				pmod_speaker = ~pmod_speaker;
-			end
-			else counter <= counter+1;
-		end
-		
-		else if (score == WL) pmod_enable = 1;
-		
-		else if (score == WR) pmod_enable = 1;
-		
-		else pmod_enable = 0;
-	end
-*/
